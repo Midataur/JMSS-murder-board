cd9f13907331cded2b6e75092c8e29ee04feb94e
Darcy LF <ehnevault@gmail.com>
2019-10-08 18:05:47 +1100

    add 404 page

--- main.py
+++ main.py
@@ -192,10 +192,10 @@ def undokill():
 
 
 
-
-
-
-
+@app.errorhandler(404)
+def page_not_found(e):
+    # note that we set the 404 status explicitly
+    return render_template('404.html'), 404
 
 
 
